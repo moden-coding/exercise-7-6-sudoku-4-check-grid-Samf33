ea966f6d494d63659bfde56eed898fc5307c6b7d
Samf33 <144719150+Samf33@users.noreply.github.com>
2025-09-02 15:26:07 -0400

    done

--- src/sudoku_grid.py
+++ src/sudoku_grid.py
@@ -1,6 +1,77 @@
 def sudoku_grid_correct(sudoku: list):
-    pass
+    curr = True
+    curr1 = True
+    count3 = 0
+    count4 = 0
+    for i in range(9):
+        curr = row_correct(sudoku, i)
+        curr1=column_correct(sudoku,i)
+        if(curr is False or curr1 is False):
+            return False
+    for row in sudoku:
+        count4=0
+        for inte in row:
+            if count3 % 3 == 0 and count4 % 3 == 0:
+                curr = block_correct(sudoku,count3,count4)
+            if curr is False:
+                return False
+            count4+=1
+        count3+=1
+    return True
+def block_correct(sudoku: list, row_no: int, column_no: int):
+    count1=0
+    count2=0
+    column=[]
+    for val in sudoku:
+        count2=0
+        for valu in val:
+            if(count1 >= row_no and count1 < row_no +3 and count2 >= column_no and count2<column_no+3):
+                column.append(valu)
+            count2+=1
+        count1+=1
 
+    count1 = 0
+    count2 = 0
+    for num in column:
+        count1 += 1
+        for nom in column:
+            count2+=1
+            if nom == num and nom != 0 and num != 0 and count1 != count2:
+                return False
+        count2 = 0
+    return True
+
+def column_correct(list : list, column_num : int):
+    column = []
+    count1 = 0
+    for row in list:
+        count1 = 0
+        for value in row:
+            if count1 == column_num:
+                column.append(value)
+            count1 +=1
+    count1 = 0
+    count2 = 0
+    for num in column:
+        count1 += 1
+        for nom in column:
+            count2+=1
+            if nom == num and nom != 0 and num != 0 and count1 != count2:
+                return False
+        count2 = 0
+    return True
+
+def row_correct(sudoku : list, row_index : int):
+    count1 = 0
+    count2 = 0
+    for num in sudoku[row_index]:
+        count1 += 1
+        for nom in sudoku[row_index]:
+            count2+=1
+            if nom == num and nom != 0 and num != 0 and count1 != count2:
+                return False
+        count2 = 0
+    return True
 if __name__ == "__main__":
 
     sudoku1 = [
